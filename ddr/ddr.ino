1c07335de771727bb1c4b5534cd42f8f5aa022e6
Elyas-A <alhaddad.elyas@gmail.com>
2025-06-10 21:06:59 -0700

    Fixed bugs.

--- ddr/ddr.ino
+++ ddr/ddr.ino
@@ -10,19 +10,14 @@ Servo U_SRVO;
 Servo D_SRVO;
 
 int SRVO_LIFT = 90;
-int SRVO_PUSH = 60;
+int SRVO_PUSH = 100;
 
-int L_SRVO_PIN = 3;
-int R_SRVO_PIN = 5;
-int U_SRVO_PIN = 6;
-int D_SRVO_PIN = 9;
-int S_BTTN_PIN = 12;
-int LED_PINS[] = {10, 11};
-int S_BTTN;
-
-int L_FOOT_COLOR[] = {255, 0};
-int R_FOOT_COLOR[] = {0, 255};
-int B_FEET_COLOR[] = {255, 255};
+int L_SRVO_PIN = 6;
+int R_SRVO_PIN = 10;
+int U_SRVO_PIN = 11;
+int D_SRVO_PIN = 5;
+int R_PIN = 8;
+int B_PIN = 7;
 
 void setup() {
     // put your setup code here, to run once:
@@ -31,26 +26,23 @@ void setup() {
     R_SRVO.attach(R_SRVO_PIN);
     U_SRVO.attach(U_SRVO_PIN);
     D_SRVO.attach(D_SRVO_PIN);
-    pinMode(S_BTTN_PIN, INPUT);
+    pinMode(R_PIN, OUTPUT);
+    pinMode(B_PIN, OUTPUT);
 
     // initialize sevos to default position
     clear_pad();
-
+    
     play_song();
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
-    // int S_BTTN = digitalRead(S_BTTN);
-    // if (S_BTTN == 1) {
-    //     play_song();
-    // }
-    // delay(10);
+
 }
 
 void play_song() {
-    for (int i=0; i < song_map_len; i++) {
-        if (!(song_map[i][0] == 0 && song_map[i][2] == 0)) { // checks that this is not a holding input
+    for (int i=0; i < song_map_len; i++) { 
+        if (!(song_map[i][0] == 0 && song_map[i][2] == 1)) { // checks that this is not a holding input
             clear_pad(); // since we're not holding reset the pad
             if (song_map[i][0] > 9) { // double arrow press
                 // in a double arrow value, the first digit is one foot, and the second is another,
@@ -65,6 +57,7 @@ void play_song() {
         }
         delay((60000/BPM)/LPB);
     }
+    clear_pad();
 }
 
 void move_arrow(int x) {
@@ -72,32 +65,39 @@ void move_arrow(int x) {
         case 1:
             // Up
             U_SRVO.write(SRVO_PUSH);
+            break;
         case 2:
             // right
             R_SRVO.write(SRVO_PUSH);
+            break;
         case 3:
             // down
             D_SRVO.write(SRVO_PUSH);
+            break;
         case 4:
             // left
             L_SRVO.write(SRVO_PUSH);
+            break;
     }
 }
 
 void set_led(int x) {
     switch (x) {
         case 1:
-            // left
-            analogWrite(LED_PINS[0], L_FOOT_COLOR[0]);
-            analogWrite(LED_PINS[1], L_FOOT_COLOR[1]);
+            // left foot
+            digitalWrite(R_PIN, HIGH);
+            digitalWrite(B_PIN, LOW);
+            break;
         case 2:
-            // right
-            analogWrite(LED_PINS[0], R_FOOT_COLOR[0]);
-            analogWrite(LED_PINS[1], R_FOOT_COLOR[1]);
+            // right foot
+            digitalWrite(B_PIN, HIGH);
+            digitalWrite(R_PIN, LOW);
+            break;
         case 3:
             // both
-            analogWrite(LED_PINS[0], B_FEET_COLOR[0]);
-            analogWrite(LED_PINS[1], B_FEET_COLOR[1]);
+            digitalWrite(B_PIN, HIGH);
+            digitalWrite(R_PIN, HIGH);
+            break;
     }
 }
 
@@ -106,6 +106,6 @@ void clear_pad() {
     L_SRVO.write(SRVO_LIFT);
     R_SRVO.write(SRVO_LIFT);
     D_SRVO.write(SRVO_LIFT);
-    analogWrite(LED_PINS[0], 0);
-    analogWrite(LED_PINS[1], 0);
+    digitalWrite(R_PIN, LOW);
+    digitalWrite(B_PIN, LOW);
 }
